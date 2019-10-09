df649620d1f4e88abcff9796fc8dbc66fab411ab
thibault <thibault789@gmail.com>
2019-10-09 19:51:23 +0200

    Added Nav bar and Tutorial example

--- we-verify-app/src/App.js
+++ we-verify-app/src/App.js
@@ -4,11 +4,11 @@ import './App.css';
 import NavBar from "./components/NavBar/NavBar";
 
 function App() {
-  return (
-    <div className="App">
-        <NavBar/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <NavBar/>
+        </div>
+    );
 }
 
 export default App;
