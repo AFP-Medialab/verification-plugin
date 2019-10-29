ca84ce57060b348714cfa034f3c1ecdfb723900e
thibault <thibault.lhuillier@epita.fr>
2019-10-29 11:40:55 +0100

    cleaned imports

--- src/components/ScrollTop/ScrollTop.js
+++ src/components/ScrollTop/ScrollTop.js
@@ -1,15 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
-import Box from '@material-ui/core/Box';
-import Container from '@material-ui/core/Container';
-import Fab from '@material-ui/core/Fab';
-import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
 import Zoom from '@material-ui/core/Zoom';
 
 const useStyles = makeStyles(theme => ({
