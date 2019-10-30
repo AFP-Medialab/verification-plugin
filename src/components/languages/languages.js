f3dfd6c3d8df936d9839aff737cee01de91098f2
thibault <thibault.lhuillier@epita.fr>
2019-10-30 15:45:28 +0100

    cleaned directories

--- src/components/languages/languages.js
+++ src/components/languages/languages.js
@@ -3,7 +3,7 @@ import Button from '@material-ui/core/Button';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import {useSelector, useDispatch} from "react-redux";
-import {changeLanguage} from "../../actions";
+import {changeLanguage} from "../../redux/actions";
 import loadTSVFile from "./loadTSVFile";
 
 const Languages = () => {
