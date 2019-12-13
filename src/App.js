d4fce04a84059842fd648a216f7db1a9b34fd0f5
Thibault <thibault@Thibaults-MacBook-Pro.local>
2019-12-13 11:18:14 +0100

    fixed popup

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ function App() {
                 <Switch>
                     <Route exact path={"/"} component={PopUp}/>
                     <Route path={"/app"} component={NavBar}/>
-                    <Route component={PopUp}/>
+                    <Route component={NotFound}/>
                 </Switch>
             </MuiThemeProvider>
         </Router>
