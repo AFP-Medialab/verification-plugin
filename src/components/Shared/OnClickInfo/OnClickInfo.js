2a4f1a766028e422eb07c4a83f75c479e06484c0
Thibault <thibault@Thibaults-MacBook-Pro.local>
2019-12-18 14:38:33 +0100

    changed magnifier

--- src/components/Shared/OnClickInfo/OnClickInfo.js
+++ src/components/Shared/OnClickInfo/OnClickInfo.js
@@ -5,14 +5,14 @@ import WbIncandescentIcon from '@material-ui/icons/WbIncandescent';
 import useLoadLanguage from "../../../Hooks/useLoadLanguage";
 import tsv from "../../../LocalDictionary/components/Shared/OnClickInfo.tsv";
 
-const OnClickInfo = () => {
+const OnClickInfo = (props) => {
     const classes = useMyStyles();
     const keyword = useLoadLanguage("components/Shared/OnClickInfo.tsv", tsv);
 
     return (
         <div className={classes.onClickInfo}>
             <WbIncandescentIcon fontSize={"large"}/>
-            <Typography variant={"body2"}>{keyword("keyframes_tip")}</Typography>
+            <Typography variant={"body2"}>{keyword(props.keyword)}</Typography>
         </div>
     )
 };
