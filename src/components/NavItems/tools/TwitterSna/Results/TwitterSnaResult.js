efacae068adcaf935a73e0e53155e707cbbe9172
juliette <juliette.casteran@epita.fr>
2020-01-10 11:42:01 +0100

    SNA heatmap: change buttons

--- src/components/NavItems/tools/TwitterSna/Results/TwitterSnaResult.js
+++ src/components/NavItems/tools/TwitterSna/Results/TwitterSnaResult.js
@@ -571,7 +571,7 @@ export default function TwitterSnaResult(props) {
                                         <Plot
                                          style={{ width: '100%', height: "450px" }}
                                          data={result.heatMap.plot}
-                                         
+                                         config={result.histogram.config}
                                          onClick={(e) => onHeatMapClick(e)}
                                         />
                                     }
