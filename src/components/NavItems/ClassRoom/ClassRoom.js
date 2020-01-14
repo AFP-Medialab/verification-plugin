13ea67e697c014eb25fa4bab1229b405d170f03b
thibault <thibault789@gmail.com>
2020-01-14 12:24:18 +0100

    added glossary to classRoom

--- src/components/NavItems/ClassRoom/ClassRoom.js
+++ src/components/NavItems/ClassRoom/ClassRoom.js
@@ -54,145 +54,182 @@ function a11yProps(index) {
 }
 
 const ClassRoom = () => {
-    const classes = useMyStyles();
-    const keyword = useLoadLanguage("components/NavItems/ClassRoom.tsv", tsv);
+        const classes = useMyStyles();
+        const keyword = useLoadLanguage("components/NavItems/ClassRoom.tsv", tsv);
 
-    const [value, setValue] = React.useState(0);
+        const [value, setValue] = React.useState(0);
 
-    const handleChange = (event, newValue) => {
-        setValue(newValue);
-    };
+        const handleChange = (event, newValue) => {
+            setValue(newValue);
+        };
 
-    const [videoUrl, setVideoUrl] = useState(null);
-    const [inputRef, setInputRef] = useState(null);
+        const [videoUrl, setVideoUrl] = useState(null);
+        const [inputRef, setInputRef] = useState(null);
 
-    const EducationalResources = () => {
-        let res = [];
-        for (let i = 1; keyword("classroom_title_" + i) !== ""; i++) {
-            res.push(
-                {
-                    title: keyword("classroom_title_" + i),
-                    url: keyword("classroom_url_" + i)
-                }
-            );
-        }
-        return res;
-    };
 
-    return (
-        <Paper className={classes.root}>
-            <Box justifyContent="center" display="flex" flexDirection="column">
-                <CustomTile text={keyword("classroom_title")}/>
-                <Box m={1}/>
-                <Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
-                    <Tab label={keyword("remote_resources_title")} {...a11yProps(0)} />
-                    <Tab label={keyword("user_resources_title")}  {...a11yProps(1)} />
-                </Tabs>
-                <TabPanel value={value} index={0}>
-                    <Divider/>
-                    {
-                        EducationalResources().map((value, index) => {
-                            return (
-                                <div key={index}>
-                                    <Box m={1}/>
-                                    <Grid key={index} container justify="space-between" spacing={2}
-                                          alignContent={"center"}>
-                                        <Grid item>
-                                            <CastForEducationIcon fontSize={"large"}/>
-                                        </Grid>
-                                        <Grid item align="left">
-                                            <Typography variant={"h6"}>
-                                                {
-                                                    value.title
-                                                }
-                                            </Typography>
-                                        </Grid>
-                                        <Grid item align="right">
-                                            <Button
-                                                variant="contained"
-                                                color="primary"
-                                                onClick={() => setVideoUrl(value.url)}>
-                                                {
-                                                    keyword("display")
-                                                }
-                                            </Button>
-                                        </Grid>
-                                    </Grid>
-                                    <Divider/>
-                                </div>
-                            )
-                        })
+        const glossary = () => {
+            let res = [];
+            let i = 1;
+            while (keyword("glossary_word_" + i) !== '') {
+                res.push({
+                        word: keyword("glossary_word_" + i),
+                        definition: keyword("glosary_definition_" + i)
                     }
-                </TabPanel>
-                <TabPanel value={value} index={1}>
-                    <Typography variant={"h5"}>{keyword("user_resources_intro")}</Typography>
-                    <Typography variant="body1">{keyword("user_resources_intro_remote")}</Typography>
-                    <div className={classes.root}>
+                );
+                i++;
+            }
+            return res;
+        };
 
-                        <TextField
-                            inputRef={ref => setInputRef(ref)}
-                            id="standard-full-width"
-                            label={keyword("api_input")}
-                            placeholder="URL"
-                            fullWidth
-                        />
-                        <Box m={1}/>
-                        <Button
-                            variant="contained"
-                            color="primary"
-                            onClick={() => setVideoUrl(inputRef.value)}>
+        const EducationalResources = () => {
+            let res = [];
+            for (let i = 1; keyword("classroom_title_" + i) !== ""; i++) {
+                res.push(
+                    {
+                        title: keyword("classroom_title_" + i),
+                        url: keyword("classroom_url_" + i)
+                    }
+                );
+            }
+            return res;
+        };
+
+        return (
+            <Paper className={classes.root}>
+                <Box justifyContent="center" display="flex" flexDirection="column">
+                    <CustomTile text={keyword("classroom_title")}/>
+                    <Box m={1}/>
+                    <Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
+                        <Tab label={keyword("remote_resources_title")} {...a11yProps(0)} />
+                        <Tab label={keyword("user_resources_title")}  {...a11yProps(1)} />
+                        <Tab label={keyword("glossary_title")}  {...a11yProps(2)} />
+                    </Tabs>
+                    <TabPanel value={value} index={0}>
+                        <Divider/>
+                        {
+                            EducationalResources().map((value, index) => {
+                                return (
+                                    <div key={index}>
+                                        <Box m={1}/>
+                                        <Grid key={index} container justify="space-between" spacing={2}
+                                              alignContent={"center"}>
+                                            <Grid item>
+                                                <CastForEducationIcon fontSize={"large"}/>
+                                            </Grid>
+                                            <Grid item align="left">
+                                                <Typography variant={"h6"}>
+                                                    {
+                                                        value.title
+                                                    }
+                                                </Typography>
+                                            </Grid>
+                                            <Grid item align="right">
+                                                <Button
+                                                    variant="contained"
+                                                    color="primary"
+                                                    onClick={() => setVideoUrl(value.url)}>
+                                                    {
+                                                        keyword("display")
+                                                    }
+                                                </Button>
+                                            </Grid>
+                                        </Grid>
+                                        <Divider/>
+                                    </div>
+                                )
+                            })
+                        }
+                    </TabPanel>
+                    <TabPanel value={value} index={1}>
+                        <Typography variant={"h5"}>{keyword("user_resources_intro")}</Typography>
+                        <Typography variant="body1">{keyword("user_resources_intro_remote")}</Typography>
+                        <div className={classes.root}>
+
+                            <TextField
+                                inputRef={ref => setInputRef(ref)}
+                                id="standard-full-width"
+                                label={keyword("api_input")}
+                                placeholder="URL"
+                                fullWidth
+                            />
+                            <Box m={1}/>
+                            <Button
+                                variant="contained"
+                                color="primary"
+                                onClick={() => setVideoUrl(inputRef.value)}>
+                                {
+                                    keyword("display")
+                                }
+                            </Button>
+                        </div>
+                        <Typography variant="body1" color="textSecondary">
                             {
-                                keyword("display")
+                                keyword("examples")
                             }
+                        </Typography>
+                        <Typography variant="body1" color="textSecondary">
+                            {
+                                keyword("youtube_example")
+                            }
+                        </Typography>
+                        <Typography variant="body1" color="textSecondary">
+                            {
+                                keyword("twitter_example")
+                            }
+                        </Typography>
+                        <Typography variant="body1" color="textSecondary">
+                            {
+                                keyword("website_example")
+                            }
+                        </Typography>
+                    </TabPanel>
+                    <TabPanel value={value} index={2}>
+                        {
+                            glossary().map((obj, key) => {
+                                return (
+                                    <div key={key}>
+                                        <Typography variant="h6" display="inline">
+                                            {
+                                                obj.word +" : "
+                                            }
+                                        </Typography>
+                                        <Typography variant="body1" display="inline">
+                                            {
+                                                obj.definition
+                                            }
+                                        </Typography>
+                                    </div>
+                                )
+                            })
+                        }
+                    </TabPanel>
+                </Box>
+                <img src={youCheckImage} width={"20%"} alt={youCheckImage}/>
+                <Dialog
+                    height={"400px"}
+                    fullWidth
+                    maxWidth={"md"}
+                    open={videoUrl !== null}
+                    onClose={() => setVideoUrl(null)}
+                    aria-labelledby="max-width-dialog-title"
+                >
+                    <DialogContent>
+                        <Iframe
+                            frameBorder="0"
+                            url={videoUrl}
+                            allow="fullscreen"
+                            height="400"
+                            width="100%"
+                        />
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={() => setVideoUrl(null)} color="primary">
+                            {keyword("close")}
                         </Button>
-                    </div>
-                    <Typography variant="body1" color="textSecondary">
-                        {
-                            keyword("examples")
-                        }
-                    </Typography>
-                    <Typography variant="body1" color="textSecondary">
-                        {
-                            keyword("youtube_example")
-                        }
-                    </Typography>
-                    <Typography variant="body1" color="textSecondary">
-                        {
-                            keyword("twitter_example")
-                        }
-                    </Typography>
-                    <Typography variant="body1" color="textSecondary">
-                        {
-                            keyword("website_example")
-                        }
-                    </Typography>
-                </TabPanel>
-            </Box>
-            <img src={youCheckImage} width={"20%"} alt={youCheckImage}/>
-            <Dialog
-                height={"400px"}
-                fullWidth
-                maxWidth={"md"}
-                open={videoUrl !== null}
-                onClose={() => setVideoUrl(null)}
-                aria-labelledby="max-width-dialog-title"
-            >
-                <DialogContent>
-                    <Iframe
-                        frameBorder="0"
-                        url={videoUrl}
-                        allow="fullscreen"
-                        height="400"
-                        width="100%"
-                    />
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={() => setVideoUrl(null)} color="primary">
-                        {keyword("close")}
-                    </Button>
-                </DialogActions>
-            </Dialog>
-        </Paper>
-    );
-};
+                    </DialogActions>
+                </Dialog>
+            </Paper>
+        );
+    }
+;
 export default ClassRoom;
