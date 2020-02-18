93d8dc0de9d53c308b1f4729afffef8aa7f3e8fc
Eric SCHAEFFER <eric.schaeffer@rickspirit.io>
2020-02-18 14:51:27 +0100

    Start using the authentication API functions.

--- src/components/Shared/Authentication/AuthenticationCard.js
+++ src/components/Shared/Authentication/AuthenticationCard.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { useStore, useSelector, useDispatch } from 'react-redux';
 
 import useMyStyles from "../MaterialUiStyles/useMyStyles";
+
+import authenticationAPI from "./authenticationAPI";
+import { ERR_AUTH_UNKNOWN_ERROR } from './authenticationErrors';
+import { setError } from "../../../redux/actions/errorActions";
+import useLoadLanguage from "../../../Hooks/useLoadLanguage";
+import tsv from "../../../LocalDictionary/components/Shared/Authentication.tsv";
 
 import Card from "@material-ui/core/Card";
 // import CardContent from '@material-ui/core/CardContent';
@@ -23,6 +31,48 @@ import LockOpenIcon from '@material-ui/icons/LockOpen';
 const AuthenticationCard = (props) => {
   // Defs
   const classes = useMyStyles();
+
+  // Redux store
+  const store = useStore();
+  const dispatch = useDispatch();
+
+  // Events functions
+  const messageI18NResolver = useLoadLanguage("components/Shared/Authentication.tsv", tsv);
+
+  // Error handler
+  const handleError = (errorKey) => {
+    if (messageI18NResolver(errorKey) !== "") {
+      dispatch(setError((messageI18NResolver(errorKey))));
+    } else {
+      dispatch(setError(messageI18NResolver(ERR_AUTH_UNKNOWN_ERROR)));
+    }
+  };
+
+  // Authentication Redux state
+  const userSession = useSelector(state => state.userSession);
+
+  // User Registration form
+  const [regEmail, setRegEmail] = useState(null);
+
+  // Access Code form
+  const [acEmail, setACEmail] = useState(null);
+  const acOnSubmit = () => {
+    authenticationAPI.requestAccessCode(store, {
+        email: acEmail
+    }).catch(error => {
+      handleError(error.error ? error.error.code : ERR_AUTH_UNKNOWN_ERROR);
+    });
+  }
+
+  // Login form
+  const [loginAccessCode, setLoginAccessCode] = useState(null);
+  const loginOnSubmit = () => {
+    authenticationAPI.login(store, {
+      accessCode: loginAccessCode
+    }).catch(error => {
+      handleError(error.error ? error.error.code : ERR_AUTH_UNKNOWN_ERROR);
+    });
+  }
 
   return (
     <Card raised={false} elevation={0}>
@@ -92,11 +142,12 @@ const AuthenticationCard = (props) => {
                           label="Email address"
                           required
                           fullWidth
+                          onChange={e => setACEmail(e.target.value)}
                       />
                   </Grid>
                   <Grid item xs={12}>
                       <Box mt={2}>
-                          <Button variant="contained" color="primary" startIcon={<SendIcon />}>
+                          <Button variant="contained" color="primary" startIcon={<SendIcon />} onClick={acOnSubmit}>
                               Get an access code
                           </Button>
                       </Box>
@@ -112,11 +163,12 @@ const AuthenticationCard = (props) => {
                           label="Access code"
                           required
                           fullWidth
+                          onChange={e => setLoginAccessCode(e.target.value)}
                       />
                   </Grid>
                   <Grid item xs={12}>
                       <Box mt={2}>
-                          <Button variant="contained" color="primary" startIcon={<LockOpenIcon />}>
+                          <Button variant="contained" color="primary" startIcon={<LockOpenIcon />} onClick={loginOnSubmit}>
                               Log in
                           </Button>
                       </Box>
