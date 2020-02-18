d5feeb259e58a308abe4ee14ad9f6c315ef0682e
Eric SCHAEFFER <eric.schaeffer@rickspirit.io>
2020-02-18 19:34:54 +0100

    Add user session expired redux action.

--- src/redux/reducers/authenticationReducer.js
+++ src/redux/reducers/authenticationReducer.js
@@ -2,14 +2,7 @@
  * Authentication Redux reducer function.
  */
 
-import {
-  AUTH_USER_REGISTRATION_SENT,
-  AUTH_ACCESS_CODE_REQUEST_SENT,
-  AUHT_USER_LOGIN,
-  AUHT_USER_LOGOUT,
-  AUTH_TOKEN_INVALID,
-  AUTH_TOKEN_REFRESHED
-} from "../actions/authenticationActions";
+import { AUTH_USER_REGISTRATION_SENT, AUTH_ACCESS_CODE_REQUEST_SENT, AUHT_USER_LOGIN, AUHT_USER_LOGOUT, AUTH_TOKEN_INVALID, AUTH_TOKEN_REFRESHED, AUTH_USER_SESSION_EXPIRED } from "../actions/authenticationActions";
 
 const defaultState = {
   userRegistrationSent: false,
@@ -17,14 +10,14 @@ const defaultState = {
   userAuthenticated: false,
   accessToken: null,
   accessTokenExpiry: null,
-  user: null
-  // user: {
-  //   id: null,
-  //   firstName: null,
-  //   lastName: null,
-  //   email: null,
-  //   roles: []
-  // }
+  // user: null
+  user: {
+    id: null,
+    firstName: null,
+    lastName: null,
+    email: null,
+    roles: []
+  }
 };
 
 /**
@@ -50,6 +43,13 @@ function authenticationReducer(state = defaultState, action) {
       state.userAuthenticated = true;
       state.accessToken = action.payload.accessToken;
       state.accessTokenExpiry = action.payload.accessTokenExpiry;
+      // user: {
+      //   id: null,
+      //   firstName: null,
+      //   lastName: null,
+      //   email: null,
+      //   roles: []
+      // }
       state.user = action.payload.user;
       break;
 
@@ -58,7 +58,13 @@ function authenticationReducer(state = defaultState, action) {
       state.userAuthenticated = false;
       state.accessToken = null;
       state.accessTokenExpiry = null;
-      state.user = null;
+      state.user = {
+        id: null,
+        firstName: null,
+        lastName: null,
+        email: null,
+        roles: []
+      };
       break;
 
     case AUTH_TOKEN_INVALID:
@@ -68,7 +74,21 @@ function authenticationReducer(state = defaultState, action) {
     case AUTH_TOKEN_REFRESHED:
       // TODO
       break;
-    }
+
+    case AUTH_USER_SESSION_EXPIRED:
+      // TODO
+      state.userAuthenticated = false;
+      state.accessToken = null;
+      state.accessTokenExpiry = null;
+      state.user = {
+        id: null,
+        firstName: null,
+        lastName: null,
+        email: null,
+        roles: []
+      };
+      break;
+  }
 
     return state;
 };
