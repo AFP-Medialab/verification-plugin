db3566723ddd9cb1483455825d7095d7b02e4305
Eric SCHAEFFER <eric.schaeffer@rickspirit.io>
2020-02-27 02:27:19 +0100

    Auto login from "ac" query parameter.

--- src/App.js
+++ src/App.js
@@ -1,93 +1,115 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import NavBar from "./components/NavBar/NavBar";
-import {MuiThemeProvider} from "@material-ui/core";
+import { MuiThemeProvider } from "@material-ui/core";
 import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
-import {HashRouter as Router, Route, Switch} from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import history from "./components/Shared/History/History";
 import PopUp from "./components/PopUp/PopUp";
 import ReactGA from 'react-ga';
-import {useSelector} from "react-redux";
+import { useSelector } from "react-redux";
+import { userLoginAction } from './redux/actions/authenticationActions';
 //import auth from './auth.ts'; // Sample authentication provider
+import useAuthenticationAPI from './components/Shared/Authentication/useAuthenticationAPI';
 
 const theme = createMuiTheme({
-    palette: {
-        primary: {
-            light: '#fff',
-            main: 'rgb(0,170,180)',
-            dark: '#00707e'
-        },
-        secondary: {
-            main: '#ffaf33',
-        },
-        error: {
-            main: 'rgb(198,57,59)'
-        }
+  palette: {
+    primary: {
+      light: '#fff',
+      main: 'rgb(0,170,180)',
+      dark: '#00707e'
     },
-    typography: {
-        useNextVariants: 'true',
+    secondary: {
+      main: '#ffaf33',
     },
-    overrides: {
-        MuiButton: {
-            containedPrimary: {
-                color: 'white',
-            },
-        },
-        MuiIcon: {
-            root: {
-                overflow: "visible"
-            }
-        }
-    },
-    zIndex: {
-        drawer: 1099
+    error: {
+      main: 'rgb(198,57,59)'
     }
+  },
+  typography: {
+    useNextVariants: 'true',
+  },
+  overrides: {
+    MuiButton: {
+      containedPrimary: {
+        color: 'white',
+      },
+    },
+    MuiIcon: {
+      root: {
+        overflow: "visible"
+      }
+    }
+  },
+  zIndex: {
+    drawer: 1099
+  }
 });
 
 const NotFound = () => {
-    return (
-        <div>404 not found</div>
-    )
+  return (
+    <div>404 not found</div>
+  );
 };
 
 function App() {
 
-    const cookies = useSelector(state => state.cookies);
+  const cookies = useSelector(state => state.cookies);
 
-    useEffect(() => {
-        const trackingId = process.env.REACT_APP_GOOGLE_ANALYTICS_KEY;
-        ReactGA.initialize(trackingId, {
-            //debug: true,
-            titleCase: false,
-        });
-        ReactGA.ga('set', 'checkProtocolTask', () => {
-        });
-        ReactGA.pageview('/popup.html');
-        history.listen(location => {
-            ReactGA.set({page: location.pathname}); // Update the user's current page
-            ReactGA.pageview(location.pathname); // Record a pageview for the given page
-        });
-    }, []);
+  useEffect(() => {
+    const trackingId = process.env.REACT_APP_GOOGLE_ANALYTICS_KEY;
+    ReactGA.initialize(trackingId, {
+      //debug: true,
+      titleCase: false,
+    });
+    ReactGA.ga('set', 'checkProtocolTask', () => {
+    });
+    ReactGA.pageview('/popup.html');
+    history.listen(location => {
+      ReactGA.set({ page: location.pathname }); // Update the user's current page
+      ReactGA.pageview(location.pathname); // Record a pageview for the given page
+    });
+  }, []);
 
-    useEffect(() => {
-        if (cookies !== null && cookies) {
-            window['ga-disable-' + process.env.REACT_APP_GOOGLE_ANALYTICS_KEY] = false;
-        } else {
-            window['ga-disable-' + process.env.REACT_APP_GOOGLE_ANALYTICS_KEY] = true;
-        }
+  useEffect(() => {
+    if (cookies !== null && cookies) {
+      window['ga-disable-' + process.env.REACT_APP_GOOGLE_ANALYTICS_KEY] = false;
+    } else {
+      window['ga-disable-' + process.env.REACT_APP_GOOGLE_ANALYTICS_KEY] = true;
+    }
 
-    }, [cookies]);
+  }, [cookies]);
 
-    return (
-        <Router history={history}>
-            <MuiThemeProvider theme={theme}>
-                <Switch>
-                    <Route exact path={"/"} component={PopUp}/>
-                    <Route path={"/app"} component={NavBar}/>
-                    <Route component={NotFound}/>
-                </Switch>
-            </MuiThemeProvider>
-        </Router>
-    );
+  const authenticationAPI = useAuthenticationAPI();
+
+  // Handle authentication link with access code
+  // console.log("Location: ", window.location);
+  // const locationSearch = window.location.search;
+  // console.log("Query params: ", locationSearch);
+  const locationSearchStart = window.location.href.lastIndexOf("?");
+  if (locationSearchStart > 0) {
+    const locationSearch = window.location.href.substring(locationSearchStart + 1);
+    // console.log("Query params: ", locationSearch);
+    if (locationSearch) {
+      const locationParams = new URLSearchParams(locationSearch);
+      const accessCode = locationParams.get('ac');
+      // console.log("Found ac param: ", accessCode);
+      if (accessCode) {
+        authenticationAPI.login({ accessCode });
+      }
+    }
+  }
+
+  return (
+    <Router history={history}>
+      <MuiThemeProvider theme={theme}>
+        <Switch>
+          <Route exact path={"/"} component={PopUp} />
+          <Route path={"/app"} component={NavBar} />
+          <Route component={NotFound} />
+        </Switch>
+      </MuiThemeProvider>
+    </Router>
+  );
 }
 
 export default App;
