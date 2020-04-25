badd85bd663b6d43b41db86bc669c7fe84af9805
Duyen Do <dtduyenit@gmail.com>
2020-04-25 18:04:31 +0200

    Remove async from heatmap

--- src/components/NavItems/tools/TwitterSna/Hooks/useTwitterSnaRequest.js
+++ src/components/NavItems/tools/TwitterSna/Hooks/useTwitterSnaRequest.js
@@ -754,11 +754,12 @@ const useTwitterSnaRequest = (request) => {
       result.tweets = responseArrayOf7[5].tweets;
       result.histogram = createHistogram(data, responseArrayOf7[6], givenFrom, givenUntil);
       if (final) {
-        result.cloudChart = createWordCloud(responseArrayOf7[7]);
-        createHeatMap(request, responseArrayOf7[5].tweets).then((heatmap) => result.heatMap = heatmap);
-        // result.netGraph = createHashtagGraph(request, responseArrayOf7[5]);
-        result.netGraph = createHashtagGraph2(request, responseArrayOf7[5]);
         result.csvArrHashtags = createCsvArrHashtags(responseArrayOf7[5]);
+        result.cloudChart = createWordCloud(responseArrayOf7[7]);
+        result.heatMap = createHeatMap(request, responseArrayOf7[5].tweets);
+        // result.netGraph = createHashtagGraph(request, responseArrayOf7[5]);
+        // debugger;
+        result.netGraph = createHashtagGraph2(request, responseArrayOf7[5]);
       }
       else
         result.cloudChart = { title: "top_words_cloud_chart_title" };
@@ -833,7 +834,7 @@ const useTwitterSnaRequest = (request) => {
 
     };
 
-    async function createHeatMap(entries, hits) {
+    function createHeatMap(entries, hits) {
 
       let hourAxis = ['00:00', '01:00', '02:00', '03:00', '04:00', '05:00', '06:00', '07:00', '08:00', '09:00', '10:00', '11:00',
                      '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00', '23:00'];
