d4884d8aa041205f800ba33977cfa0b2da4f4198
Duyen Do <dtduyenit@gmail.com>
2020-04-26 18:35:58 +0200

    Dynamic search engines in 3 languages by putting the links to csv

--- src/components/NavItems/tools/CovidSearch/CovidSearch.js
+++ src/components/NavItems/tools/CovidSearch/CovidSearch.js
@@ -1,21 +1,10 @@
 import React, { useEffect, useState } from "react";
 
-import { useDispatch, useSelector } from "react-redux";
-import { setError } from "../../../../redux/actions/errorActions";
-
-import dateFormat from "dateformat";
 import _ from "lodash";
 
 import useMyStyles from "../../../Shared/MaterialUiStyles/useMyStyles";
 import Paper from "@material-ui/core/Paper";
-import TextField from "@material-ui/core/TextField";
-import FormControl from "@material-ui/core/FormControl";
-import RadioGroup from "@material-ui/core/RadioGroup";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Radio from "@material-ui/core/Radio";
 import Box from "@material-ui/core/Box";
-
-import SearchIcon from '@material-ui/icons/Search';
 
 import CustomTile from "../../../Shared/CustomTitle/CustomTitle";
 import useLoadLanguage from "../../../../Hooks/useLoadLanguage";
@@ -25,19 +14,11 @@ import tsv from "../../../../LocalDictionary/components/NavItems/tools/CovidSear
 const CovidSearch = () => {
   const classes = useMyStyles();
   const keyword = useLoadLanguage("components/NavItems/tools/CovidSearch.tsv", tsv);
-  console.log("kedddd", keyword);
-
-  const searchEngines = [
-    "https://cse.google.com/cse.js?cx=000556916517770601014:i0sxs8kmmzr",
-    "https://cse.google.com/cse.js?cx=000556916517770601014:jff3t29fxeq",
-    "https://cse.google.com/cse.js?cx=000556916517770601014:3imsvcumevz"
-  ]
 
   useEffect(() => {
 
-    searchEngines.forEach(engine => {
       const script = document.createElement('script');
-      script.src = engine;
+      script.src = "https://cse.google.com/cse.js?cx=000556916517770601014:" + keyword("covidsearch_engines");
       script.async = true;
     
       document.head.appendChild(script);
@@ -45,7 +26,6 @@ const CovidSearch = () => {
       return () => {
         document.head.removeChild(script);
       }
-    });
     
   }, []);
 
--- src/components/NavItems/tools/XNetwork/XNetwork.js
+++ src/components/NavItems/tools/XNetwork/XNetwork.js
@@ -1,29 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 
-import { useDispatch, useSelector } from "react-redux";
-import { setError } from "../../../../redux/actions/errorActions";
-
-import dateFormat from "dateformat";
 import _ from "lodash";
 
 import useMyStyles from "../../../Shared/MaterialUiStyles/useMyStyles";
 import Paper from "@material-ui/core/Paper";
-import TextField from "@material-ui/core/TextField";
-import FormControl from "@material-ui/core/FormControl";
-import RadioGroup from "@material-ui/core/RadioGroup";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Radio from "@material-ui/core/Radio";
 import Box from "@material-ui/core/Box";
-import FormLabel from "@material-ui/core/FormLabel";
-import Button from "@material-ui/core/Button";
-import Grid from "@material-ui/core/Grid";
-import InputLabel from "@material-ui/core/InputLabel";
-import Select from "@material-ui/core/Select";
-import MenuItem from "@material-ui/core/MenuItem";
-import LinearProgress from "@material-ui/core/LinearProgress";
-import Typography from "@material-ui/core/Typography";
-
-import SearchIcon from '@material-ui/icons/Search';
 
 import CustomTile from "../../../Shared/CustomTitle/CustomTitle";
 import useLoadLanguage from "../../../../Hooks/useLoadLanguage";
@@ -33,25 +14,18 @@ import tsv from "../../../../LocalDictionary/components/NavItems/tools/XNetwork.
 const XNetwork = () => {
   const classes = useMyStyles();
   const keyword = useLoadLanguage("components/NavItems/tools/XNetwork.tsv", tsv);
-  console.log("kedddd", keyword);
-
-  const searchEngines = [
-    "https://cse.google.com/cse.js?cx=000556916517770601014:k08mmqlnmih"
-  ]
 
   useEffect(() => {
 
-    searchEngines.forEach(engine => {
-      const script = document.createElement('script');
-      script.src = engine;
-      script.async = true;
-    
-      document.head.appendChild(script);
-    
-      return () => {
-        document.head.removeChild(script);
-      }
-    });
+    const script = document.createElement('script');
+    script.src = "https://cse.google.com/cse.js?cx=000556916517770601014:k08mmqlnmih";
+    script.async = true;
+  
+    document.head.appendChild(script);
+  
+    return () => {
+      document.head.removeChild(script);
+    }
     
   }, []);
 
