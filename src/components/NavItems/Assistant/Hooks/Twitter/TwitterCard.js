b90b06ea1dc0d2b2a4a6401d4c6f928e3ffb0eb9
muneerahp <m.a.patel@sheffield.ac.uk>
2020-04-29 11:37:33 +0100

    Revert "parameterizing the db of known  fakes query"
    This reverts commit a93bdaf8814b56daca646bea4caf97bb975253cf.

--- src/components/NavItems/Assistant/Hooks/Twitter/TwitterCard.js
+++ src/components/NavItems/Assistant/Hooks/Twitter/TwitterCard.js
@@ -41,38 +41,31 @@ const TwitterCard = (props) => {
     useTwint().runTwintQuery(twitterUrl);
 
     const dbPress = () => {
-
-        const inputForGpl = tweetTag.substring(1);
-        //const inputForGpl = "covid";
         setDbPressed(true);
-        const query = gql`
-              query claim($name: String) {
-                  claim(where: {AND: [{name: {IRE: $name}}, {dateCreated: {GT: "2020-01-01T00:00:00"}}]}) {
-                    id
-                    name
-                    dateCreated
-                    review {
-                      dateCreated
-                      reviewBody 
-                    }
-                    appearance{
-                      url
-                    }
-                  }
-                }`;
-
-            client.query({
-                query: query,
-                variables:{name: inputForGpl}
+        client
+            .query({
+                query: gql`
+         {
+          claim (where: {dateCreated: {GT: "2020-01-01T00:00:00"}}){ 
+            id
+            name
+            dateCreated
+            review (where: {reviewBody: {IRE: "tweetTag"}}){
+              dateCreated
+              reviewBody 
+            }
+            appearance{
+              url
+            }
+          }}`
             }).then(result => {
-                    let claims = result.data.claim;
-                    let claimedList = claims.map(claim => {
-                        return [claim.name, claim.appearance[0] == null ? "" : claim.appearance[0].url ]
-                    });
-                        console.log(claimedList);
-                        setResult(claimedList.slice(0,4))}
-                    );
-        };
+            let claims = result.data.claim;
+            let claimedList = claims.map(claim => {
+                return [claim.name, claim.appearance[0] == null ? "" : claim.appearance[0].url ]
+            });
+                console.log(claimedList);
+                setResult(claimedList.slice(0,4))}
+            );};
 
 
     return <Paper className={classes.root}>
@@ -139,10 +132,10 @@ const TwitterCard = (props) => {
                         <Card variant = "outlined" hidden = {!dbPressed}>
                             <CardContent>
                                 <Typography variant="h5" component="h2">
-                                    Appearances in the DBKF
+                                    Appearances
                                 </Typography>
                                 <Typography className={classes.title} color="primary">
-                                    Claims made and debunked concerning topics in this tweet have been listed. Search carried out against topic: {tweetTag}
+                                    Search carried out against: {tweetTag}
                                 </Typography>
                                 <Typography align={"left"} aria-rowspan={1}>
                                     {dbResult.map((claim) =>
