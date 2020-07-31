5c99f705acdab08c8c52188ceb6276fa11eb7b61
muneerahp <m.a.patel@sheffield.ac.uk>
2020-07-31 13:24:14 +0100

    adding some rudimentary exception handling

--- src/components/NavItems/Assistant/useAssistantApi.js
+++ src/components/NavItems/Assistant/useAssistantApi.js
@@ -5,8 +5,21 @@ export default function useAssistantApi() {
     const assistantScrapeUrl = process.env.REACT_APP_ASSISTANT_URL;
 
     const callAssistantScraper = async (urlType, userInput) => {
-        let scrapeResult = await axios.get(assistantScrapeUrl + urlType + "/" + userInput)
-        return scrapeResult.data
+        try {
+            let scrapeResult = await axios.get(assistantScrapeUrl  + urlType +  "/" + userInput)
+            if (scrapeResult.data.status === "success") {
+                return scrapeResult.data
+            }
+            else {throw new Error(scrapeResult.data.message)}
+        }
+        catch (error) {
+            if (!(error.isAxiosError)) {
+                throw new Error(error.message)
+            }
+            else{
+                throw new Error("An unexpected assistant error has occurred. If the problem persists, contact support.")
+            }
+        }
     }
 
     return {
