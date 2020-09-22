d76b8e54652c97d182a78e46ad06fe31197cd151
muneerahp <m.a.patel@sheffield.ac.uk>
2020-09-22 15:23:30 +0100

    removing chars dbkf can't handle in query

--- src/components/NavItems/Assistant/useDBKFApi.js
+++ src/components/NavItems/Assistant/useDBKFApi.js
@@ -5,6 +5,7 @@ export default function useDBKFApi() {
     const dbkfAPI = process.env.REACT_APP_DBKF_API;
 
     const callSearchApi = async (query) => {
+        query = query.replace(/[/"()’\\]/g, "")
         let searchResult = await axios.get(dbkfAPI  + "/claims?limit=20&q=" + query)
         let searchData = Object.values(searchResult.data)
         return searchData
