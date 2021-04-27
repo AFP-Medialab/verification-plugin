2e01d4b640eefe8e6e475193c3f097866d726ef6
Teyssou <denis.teyssou@afp.com>
2021-04-27 19:17:39 +0200

    updates Footer.js

--- src/components/Shared/Footer/Footer.js
+++ src/components/Shared/Footer/Footer.js
@@ -50,17 +50,17 @@ const Footer = (props) => {
             contactUs = keyword("apf_part_2");
             break;
         case "iti-borelli-afp":
-                provideBy = keyword("iti_part_1");
-                link = [keyword("iti_link"), keyword("borelli_link"), keyword("afp_link")];
-                linkLabel = keyword("iti_link_label"), ", " + keyword("borelli_link_label"), " " + keyword("and") + " " + keyword("apf_link_label");
-                contactUs = keyword("apf_part_2");
-                break;
+            provideBy = keyword("iti_part_1");
+            link = [keyword("iti_link"), keyword("borelli_link"), keyword("afp_link")];
+            linkLabel = [keyword("iti_link_label"), ", " + keyword("borelli_link_label"), " " + keyword("and") + " " + keyword("apf_link_label")];
+            contactUs = keyword("apf_part_2");
+            break;
         case "borelli-afp":
-                provideBy = keyword("borelli_part_1");
-                link = [keyword("borelli_link"), keyword("afp_link")];
-                linkLabel = keyword("borelli_link_label"), " " + keyword("and") + " " + keyword("apf_link_label");
-                contactUs = keyword("apf_part_2");
-                break;
+            provideBy = keyword("borelli_part_1");
+            link = [keyword("borelli_link"), keyword("afp_link")];
+            linkLabel = [keyword("borelli_link_label"), " " + keyword("and") + " " + keyword("apf_link_label")];
+            contactUs = keyword("apf_part_2");
+            break;
         default:
             provideBy = keyword("apf_part_1");
             link = keyword("apf_link");
