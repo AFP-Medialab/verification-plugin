f347cb6030d453ba09bf75985151361fdb9e882f
muneerahp <m.a.patel@sheffield.ac.uk>
2021-06-29 11:14:50 +0100

    adding annonymous CORS to image requests

--- src/components/NavItems/tools/Forensic/Results/ForensicResult.js
+++ src/components/NavItems/tools/Forensic/Results/ForensicResult.js
@@ -700,6 +700,7 @@ const ForensicResults = (props) => {
 
                                         <div className={classes.wrapperImageFilter}>
                                             <CardMedia
+                                                crossOrigin={"anonymous"}
                                                 component="img"
                                                 className={classes.imageUploaded}
                                                 image={imageDisplayed}
