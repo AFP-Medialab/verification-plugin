1683b750a3ea5cf19241e04d873c292138022257
muneerahp <m.a.patel@sheffield.ac.uk>
2021-07-20 17:16:53 +0100

    adding first iteration of archive button

--- src/components/NavItems/Assistant/AssistantUrlSelected.js
+++ src/components/NavItems/Assistant/AssistantUrlSelected.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {useDispatch, useSelector} from "react-redux";
 
+import ArchiveOutlinedIcon from '@material-ui/icons/ArchiveOutlined';
 import {Box, CardHeader, TextField} from "@material-ui/core";
 import Button from "@material-ui/core/Button";
 import Card from "@material-ui/core/Card";
@@ -11,6 +12,7 @@ import Typography from "@material-ui/core/Typography";
 import useMyStyles from "../../Shared/MaterialUiStyles/useMyStyles";
 import useLoadLanguage from "../../../Hooks/useLoadLanguage";
 
+import {KNOWN_LINKS} from "./AssistantRuleBook";
 import {submitInputUrl} from "../../../redux/actions/tools/assistantActions";
 import tsv from "../../../LocalDictionary/components/NavItems/tools/Assistant.tsv";
 
@@ -23,12 +25,30 @@ const AssistantUrlSelected = (props) => {
 
     //form states
     const inputUrl = useSelector(state => state.assistant.inputUrl);
+    const inputUrlType = useSelector(state => state.assistant.inputUrlType);
     const loading = useSelector(state => state.assistant.loading)
 
     //local state
     const formInput = props.formInput
     const setFormInput = (value) => props.setFormInput(value)
     const cleanAssistant = () => props.cleanAssistant()
+
+    const handleArchive = () =>{
+        let archiveUrl = inputUrl
+        switch (inputUrlType) {
+            case KNOWN_LINKS.FACEBOOK:
+                archiveUrl = "https://www.facebook.com/plugins/post.php?href=" + encodeURIComponent(inputUrl)
+                break;
+            case KNOWN_LINKS.INSTAGRAM:
+                if (inputUrl.endsWith("/")){
+                    archiveUrl = archiveUrl.substring(0, archiveUrl.length - 1)
+                }
+                archiveUrl = archiveUrl + "/?utm_source=ig_embed&amp;utm_campaign=loading"
+        }
+        navigator.clipboard.writeText(archiveUrl).then(()=>{
+            window.open("https://web.archive.org/save/" + archiveUrl, "_blank")
+        })
+    }
 
     return (
         <Box my={3} boxShadow={3}>
@@ -70,6 +90,16 @@ const AssistantUrlSelected = (props) => {
                                         </Button>}
                                 </Box>
                             </Grid>
+                            {inputUrl === null ? null :
+                            <Grid item xs={1}>
+                                <Box ml={1}>
+                                    <Button onClick={() => handleArchive()} startIcon={<ArchiveOutlinedIcon /> }>
+                                        <label>
+                                            Archive
+                                        </label>
+                                    </Button>
+                                </Box>
+                            </Grid>}
                         </Grid>
                     </Box>
                 </CardContent>
