8f18aba6932cb2a84d918deed586b7e5a41d5873
Bertrand Goupil <bertrand.goupil@afp.com>
2022-03-22 15:13:19 +0100

    #135 refactor analysis call api

--- src/components/NavItems/tools/Analysis/Analysis.js
+++ src/components/NavItems/tools/Analysis/Analysis.js
@@ -28,6 +28,7 @@ import Grid from "@material-ui/core/Grid";
 import HeaderTool from "../../../Shared/HeaderTool/HeaderTool";
 import styles from "./Results/layout.module.css";
 import Alert from '@material-ui/lab/Alert';
+import _ from "lodash";
 
 
 const Analysis = () => {
@@ -49,8 +50,7 @@ const Analysis = () => {
     const serviceUrl = "https://mever.iti.gr/caa/api/v4/videos";
     const [finalUrl, showFacebookIframe] = useGenerateApiUrl(serviceUrl, submittedUrl, reprocess);
     useAnalysisWrapper(setAnalysisLoading, setAnalysisResult, serviceUrl, finalUrl, submittedUrl, keyword);
-
-
+    
     var [warning, setWarning] = useState(false);
     
     const reprocessToggle = () => {
@@ -62,6 +62,10 @@ const Analysis = () => {
         setSubmittedUrl(input.trim());
         dispatch(cleanAnalysisState());
     };
+    useEffect(() => {
+        if(_.isEmpty(resultUrl))
+            setInput(resultUrl)
+    }, [resultUrl])
   
     useEffect(() => {       
         if (finalUrl !== undefined) {
@@ -120,19 +124,15 @@ const Analysis = () => {
                                     var found1 =e.target.value.match(regex1);
                                     if(found!==null || found1!==null ){
                                         setWarning(true)
-
                                     }
                                     else{
                                         setWarning(false)
-                                    }
-                                    
+                                    }                                    
                                 }
-
                                 }
                             />
                             
                         </Grid>
-
                         <Grid item>
                             <FormControlLabel
                                 control={
@@ -146,7 +146,6 @@ const Analysis = () => {
                                 }
                                 label={keyword("api_repro")}
                             />
-
                         </Grid>
 
                         <Grid item>
@@ -158,12 +157,8 @@ const Analysis = () => {
                                 >
                                 {keyword("button_submit")}
                             </Button>
-                        </Grid>
-
-                        
-
+                        </Grid>                        
                         <Box m={1}/>
-                        
                     </Grid>
                 </div>
                 <LinearProgress hidden={!isLoading} />
@@ -193,15 +188,11 @@ const Analysis = () => {
             {
                 (resultData  && resultData.platform.startsWith("facebook")) ?
                     <FacebookResults report={resultData}/> : null
-                    
-
             }
             
             {
-                
                     warning===true &&
                     <Alert className={styles.margin1} variant="outlined" severity="warning">{keyword("facebook_tip")}</Alert>
-
             }
                 
                 
--- src/components/NavItems/tools/Analysis/Hooks/useAnalysisWrapper.js
+++ src/components/NavItems/tools/Analysis/Hooks/useAnalysisWrapper.js
@@ -1,86 +1,100 @@
 import axios from "axios"
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {setError} from "../../../../../redux/actions/errorActions";
 import {useEffect, useState} from "react";
+import _ from "lodash";
 
 export const useAnalysisWrapper = (setAnalysisLoading, setAnalysisResult, serviceUrl, apiUrl, processUrl, keyword) => {    
     const dispatch = useDispatch();
-    const [count, setCount] = useState(0);
-    useEffect(() => {
-        const handleError = (error) => {
-            if (keyword(error) !== "")
-                dispatch(setError((keyword(error))));
-            else
-                dispatch(setError(error.toString()));
-            dispatch(setAnalysisLoading(false));
-        };
+    const isLoading = useSelector(state => state.analysis.loading);
+    const [data, setData] = useState(null)
+    const [cpt, setCpt] = useState(0)
+    const [currentURL, setCurrentURL] = useState(processUrl)
 
-        const getReport = (id, processing) => {
-            axios.get(serviceUrl+"/reports/" + id)
-                .then(response => {
-                    if (keyword("table_error_" + response.data.status) !== "")
-                        handleError("table_error_" + response.data.status.status);
-                    else if (response.data.status !== "unavailable"){
-
-                        //console.log(response.data);
-
-                        if (response.data.platform === "facebook"){
-                            axios.get("https://weverify-assistant-dev.gate.ac.uk/scrape/facebook?url=" + processUrl)
-                                .then(responseImg => {
-                                    //console.log(responseImg);
-                                    //console.log(responseImg.data.images[0]);
-                                    dispatch(setAnalysisResult(processUrl, response.data, false, processing, responseImg.data.images[0]));
-                                })
-                        }else{
-                            dispatch(setAnalysisResult(processUrl, response.data, false, processing, null));
-                        }
-                        
+    const waitUntilDonne = (data, cpt = 0) => {
+       
+        axios.get(serviceUrl+"/jobs/" + data.id)
+            .then(response => {
+                //console.log(response);
+                if (response.status === 200 && response.data.status === "done") {
+                    getReport(response.data.media_id, false)
+                } else if ( keyword("table_error_" +  response.data.status) !== "") {
+                    handleError("table_error_" + response.data.status);
+                } else if (response.data.status === "unavailable"){
+                    handleError("table_error_unavailable")
+                }
+                else {
+                    if(cpt % 10 === 1){
+                        //Every 10 requests get a report content for temporary display
+                        getReport(response.data.media_id, true)
                     }
-                })
-                .catch(errors => handleError(errors));
-        };
+                    setCpt(cpt + 1)
+                }
+            })
+            .catch(error => {
+            
+            })
+    };
+    const getReport = (id, processing) => {
+        axios.get(serviceUrl+"/reports/" + id)
+            .then(response => {
+                if (keyword("table_error_" + response.data.status) !== "")
+                    handleError("table_error_" + response.data.status.status);
+                else if (response.data.status !== "unavailable"){
+                    if (response.data.platform === "facebook"){
+                        axios.get("https://weverify-assistant-dev.gate.ac.uk/scrape/facebook?url=" + currentURL)
+                            .then(responseImg => {
+                                dispatch(setAnalysisResult(currentURL, response.data, false, processing, responseImg.data.images[0]));
+                            })
+                    }else{
+                        dispatch(setAnalysisResult(currentURL, response.data, false, processing, null));
+                    }
+                    
+                }
+            })
+            .catch(errors => handleError(errors));
+    };
+    const handleError = (error) => {
+        if (keyword(error) !== "")
+            dispatch(setError((keyword(error))));
+        else
+            dispatch(setError(error.toString()));
+        dispatch(setAnalysisLoading(false));
+    };
 
+    useEffect(()=> {
+        let timer = null
+        if(!_.isNull(data)){
+            timer = setTimeout(() => waitUntilDonne(data, cpt), 2000);
+            if(!isLoading && !_.isNull(timer)){
+                clearTimeout(timer)
+                setCpt(0)
+            }
+        }
+        return () => {
+            if(!_.isNull(timer))
+                clearTimeout(timer)
+            }
+         // eslint-disable-next-line
+    }, [isLoading, data, cpt])
+
+
+    useEffect(() => {
         const handleJob = (data) => {
             if (keyword("table_error_" + data.status) !== "") {
                 handleError("table_error_" + data.status);
             } else {
-                waitUntilDonne(data);
+                setData(data)
             }
         };
-
-
-        const waitUntilDonne = (data, cpt = 0) => {
-            axios.get(serviceUrl+"/jobs/" + data.id)
-                .then(response => {
-                    //console.log(response);
-                    if (response.status === 200 && response.data.status === "done") {
-                        getReport(response.data.media_id, false)
-                    } else if ( keyword("table_error_" +  response.data.status) !== "") {
-                        handleError("table_error_" + response.data.status);
-                    } else if (response.data.status === "unavailable"){
-                        handleError("table_error_unavailable")
-                    }
-                    else {
-                        if(cpt % 10 === 1){
-                            console.log("modulo .... ", cpt)
-                            getReport(response.data.media_id, true)
-                        }
-                        setTimeout(() => waitUntilDonne(response.data, cpt + 1), 2000);
-                    }
-                })
-                .catch(error => {
-                
-                })
-        };
-
-
         if (!apiUrl)
             return;
         if (apiUrl === "")
             handleError("table_error_empty_url");
         else if (apiUrl.includes(" "))
             handleError("table_error_unavailable");
-        else if (processUrl) {           
+        else if (processUrl) {        
+            setCurrentURL(processUrl)   
             dispatch(setAnalysisLoading(true));
             axios.post(apiUrl)
                 .then(response => handleJob(response["data"]))
