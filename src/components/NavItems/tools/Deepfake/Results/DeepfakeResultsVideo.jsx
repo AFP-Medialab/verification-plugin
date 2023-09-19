de4e844fa9cf92aba5a7962e1cde7b9591fbea36
Valentin Porcellini <valentin.porcellini@afp.com>
2023-09-19 15:40:08 +0200

    Removed fixed height

--- src/components/NavItems/tools/Deepfake/Results/DeepfakeResultsVideo.jsx
+++ src/components/NavItems/tools/Deepfake/Results/DeepfakeResultsVideo.jsx
@@ -166,7 +166,6 @@ const DeepfakeResultsVideo = (props) => {
                 borderRadius: "10px",
                 boxShadow:
                   "0px 2px 1px -1px rgb(0 0 0 / 20%), 0px 1px 1px 0px rgb(0 0 0 / 14%), 0px 1px 3px 0px rgb(0 0 0 / 12%)",
-                maxHeight: "40vh",
               }}
             >
               <source
