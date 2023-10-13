b4730810e199c8a552a785869bb2c08b27341e9a
Bertrand Goupil <bertrand.goupil@afp.com>
2023-10-13 11:35:05 +0200

    remove log

--- src/components/Shared/DateTimePicker/convertToGMT.jsx
+++ src/components/Shared/DateTimePicker/convertToGMT.jsx
@@ -1,6 +1,5 @@
 import moment from "moment-timezone";
 export const convertMomentToGMT = (datetime) => {
   let date = datetime.format("YYYY-MM-DDTHH:mm:ss").toString();
-  console.log("date ", date);
   return moment.tz(date, "UTC");
 };
