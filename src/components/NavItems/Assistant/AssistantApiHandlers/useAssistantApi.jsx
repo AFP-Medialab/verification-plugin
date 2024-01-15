4ecf36a84757c4f42cf6a90882b7b6bd9ef126eb
Bertrand Goupil <bertrand.goupil@afp.com>
2024-01-15 09:31:00 +0100

    remove defunct assistant machine translation

--- src/components/NavItems/Assistant/AssistantApiHandlers/useAssistantApi.jsx
+++ src/components/NavItems/Assistant/AssistantApiHandlers/useAssistantApi.jsx
@@ -85,7 +85,6 @@ export default function assistantApiCalls() {
     callAssistantScraper,
     callSourceCredibilityService,
     callNamedEntityService,
-    callAssistantTranslator,
     callOcrService,
     callOcrScriptService,
   };
