c05b58cbe2c1de704fa43ecfaba722781174631d
Valentin Porcellini <valentin.porcellini@afp.com>
2024-01-17 17:57:59 +0100

    wip

--- src/components/NavItems/tools/SemanticSearch/index.jsx
+++ src/components/NavItems/tools/SemanticSearch/index.jsx
@@ -9,6 +9,10 @@ import CheckboxesTags from "./components/CheckboxesTags";
 import { DatePicker } from "@mui/x-date-pickers";
 import SelectSmall from "./components/SelectSmall";
 import LoadingButton from "@mui/lab/LoadingButton";
+import Link from "@mui/material/Link";
+import Modal from "@mui/material/Modal";
+import Backdrop from "@mui/material/Backdrop";
+import Typography from "@mui/material/Typography";
 
 const SemanticSearch = () => {
   const [isLoading, setIsLoading] = useState(false);
@@ -24,11 +28,41 @@ const SemanticSearch = () => {
   ];
 
   const searchEngineModes = [
-    { name: "Automatic selection" },
-    { name: "Multilingual semantic Search" },
-    { name: "English based semantic search" },
-    { name: "English based keyword search" },
+    {
+      name: "Automatic selection",
+      description:
+        "Automatic selection will analyze the provided input and according to its language and length will automatically select the most appropriate search engine.",
+    },
+    {
+      name: "English based semantic search",
+      description:
+        "An English-based semantic search, where you can search any piece of text (a sentence, a paragraph or even a whole FB / Telegram / Twitter post) for any matching previously fact-checked claim. The input needs to be in English in this case (it can be translated by you from other languages using automatic translation such as Google Translate - if this model will perform well and we will use it  in the production app, we will add automatic translation later).",
+    },
+    {
+      name: "Multilingual semantic Search",
+      description:
+        "A Multilingual semantic search, which works the same as the one above, but should be able to work with all common languages, including CEDMO ones, i.e.,  Slovak, Czech, and Polish.",
+    },
+    {
+      name: "English based keyword search",
+      description:
+        "Finally, a simple English-based keyword-based search (a standard/baseline engine that can provide good results for shorter inputs and can serve for comparison with previous more advanced models).",
+    },
   ];
+
+  const searchEngineModalStyle = {
+    position: "absolute",
+    display: "block",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: "50vw",
+    backgroundColor: "background.paper",
+    outline: "unset",
+    borderRadius: "10px",
+    boxShadow: 24,
+    p: 4,
+  };
 
   const handleSubmit = async () => {
     setIsLoading(true);
@@ -39,6 +73,14 @@ const SemanticSearch = () => {
       setErrorMessage("This is a test error message");
     }, 5000);
   };
+
+  const [openSearchEngineModal, setOpenSearchEngineModal] =
+    React.useState(false);
+  const handleOpenSearchEngineModal = () => {
+    setOpenSearchEngineModal(true);
+  };
+
+  const handleCloseSearchEngineModal = () => setOpenSearchEngineModal(false);
 
   return (
     <Box>
@@ -69,8 +111,8 @@ const SemanticSearch = () => {
                   <TextField
                     fullWidth
                     value={input}
-                    label={"Search for a Fact Check"}
-                    placeholder={"Search for a Fact Check"}
+                    label={"Search if already fact-checked"}
+                    placeholder={"Search if already fact-checked"}
                     multiline
                     minRows={2}
                     variant="outlined"
@@ -94,11 +136,64 @@ const SemanticSearch = () => {
                 </Stack>
 
                 <Stack direction="row" spacing={2}>
-                  <SelectSmall
-                    items={searchEngineModes}
-                    initialValue={searchEngineModes[0].name}
-                    disabled={isLoading}
-                  />
+                  <Stack direction="column" spacing={1}>
+                    <SelectSmall
+                      items={searchEngineModes}
+                      initialValue={searchEngineModes[0].name}
+                      disabled={isLoading}
+                    />
+                    <Link
+                      onClick={handleOpenSearchEngineModal}
+                      sx={{ cursor: "pointer" }}
+                    >
+                      How to choose?
+                    </Link>
+                    <Modal
+                      aria-labelledby="transition-modal-title"
+                      aria-describedby="transition-modal-description"
+                      open={openSearchEngineModal}
+                      onClose={handleCloseSearchEngineModal}
+                      closeAfterTransition
+                      slots={{ backdrop: Backdrop }}
+                      slotProps={{
+                        backdrop: {
+                          timeout: 500,
+                        },
+                      }}
+                    >
+                      <Fade in={openSearchEngineModal}>
+                        <Box sx={searchEngineModalStyle}>
+                          <Typography
+                            id="transition-modal-title"
+                            variant="h6"
+                            component="h2"
+                          >
+                            How to choose the search engine?
+                          </Typography>
+                          <Stack
+                            id="transition-modal-description"
+                            direction="column"
+                            spacing={2}
+                            mt={2}
+                          >
+                            {searchEngineModes.map((searchEngine, index) => {
+                              return (
+                                <Stack direction="column" key={index}>
+                                  <Typography variant="subtitle1">
+                                    {searchEngine.name}
+                                  </Typography>
+                                  <Alert severity="info" icon={false}>
+                                    {searchEngine.description}
+                                  </Alert>
+                                </Stack>
+                              );
+                            })}
+                          </Stack>
+                        </Box>
+                      </Fade>
+                    </Modal>
+                  </Stack>
+
                   <DatePicker label="From:" disabled={isLoading} />
                   <DatePicker label="To:" disabled={isLoading} />
                   <CheckboxesTags
--- src/components/NavItems/tools/SemanticSearch/semanticSearchResults.jsx
+++ src/components/NavItems/tools/SemanticSearch/semanticSearchResults.jsx
@@ -6,51 +6,100 @@ import Card from "@mui/material/Card";
 import Link from "@mui/material/Link";
 import Avatar from "@mui/material/Avatar";
 import Divider from "@mui/material/Divider";
+import Grid from "@mui/material/Grid";
 
 const SemanticSearchResults = (searchResults) => {
   searchResults = {
     claim:
-      "Claim: A video clip shows the arrest of an Iranian sniper who killed protesters in the Green Zone in Baghdad during the recent clashes.",
-    title: "Title: This clip has been circulating since 2011...",
-    rating: "Rating: False",
+      "A video clip shows the arrest of an Iranian sniper who killed protesters in the Green Zone in Baghdad during the recent clashes.",
+    title:
+      "This clip has been circulating since 2011, and it does not show an Iranian sniper who was recently arrested in Iraq.",
+    rating: "False",
     date: "Published on 2022-08-31",
     website: "misbar.com",
     language: "Arabic",
+    similarityScore: 0.88,
+    articleUrl:
+      "https://misbar.com/factcheck/2023/11/19/الصورة-قديمة-وليست-لمقتل-قناص-إسرائيلي-عل-يد-المقاومة-في-غزة#a74d408bebaf3eef320fa8bf71817523",
+    domainUrl: "http://misbar.com/",
   };
 
-  const result = (claim, title, rating, date, website, language) => {
+  const result = (
+    claim,
+    title,
+    rating,
+    date,
+    website,
+    language,
+    similarityScore,
+    articleUrl,
+    domainUrl,
+  ) => {
     return (
-      <Box>
+      <Box width="100%">
+        <Grid container direction="row" p={2} justifyContent="space-between">
+          <Grid
+            item
+            container
+            direction="row"
+            xs={10}
+            spacing={2}
+            justifyContent="flex-start"
+            alignItems="flex-start"
+          >
+            <Grid item>
+              <Avatar
+                src="https://sprawdzam.afp.com/sites/default/files/styles/twitter_card/public/medias/factchecking/g2/2022-09/aa2dc4ac30e8a183448986495b465e19.jpeg?itok=GLOsh8mv"
+                variant="rounded"
+                sx={{ width: 80, height: 80 }}
+              />
+            </Grid>
+            <Grid item xs>
+              <Stack
+                direction="column"
+                justifyContent="flex-start"
+                alignItems="flex-start"
+                spacing={2}
+              >
+                <Stack direction="column">
+                  <Typography>Claim: {claim}</Typography>
+                  <Typography variant="caption">
+                    Translated from {language} •{" "}
+                    <Link sx={{ cursor: "pointer" }}>See Original</Link>
+                  </Typography>
+                </Stack>
+                <Stack direction="column">
+                  <Typography>
+                    Title: <Link href={articleUrl}>{title}</Link>
+                  </Typography>
+
+                  <Typography variant="caption">
+                    Translated from {language} •{" "}
+                    <Link sx={{ cursor: "pointer" }}>See Original</Link>
+                  </Typography>
+                </Stack>
+                <Typography variant="body2">Rating: {rating}</Typography>
+                <Typography variant="subtitle2">{date}</Typography>
+              </Stack>
+            </Grid>
+          </Grid>
+          <Grid item xs={2} pl={4}>
+            <Stack direction="column" spacing={2}>
+              <Link href={domainUrl} variant="body1">
+                {website}
+              </Link>
+              <Chip label={language} sx={{ width: "fit-content" }} />
+              <Typography variant="body2">Score: {similarityScore}</Typography>
+            </Stack>
+          </Grid>
+        </Grid>
         <Stack
           direction="row"
           spacing={2}
           p={2}
           justifyContent="flex-start"
           alignItems="flex-start"
-        >
-          <Avatar
-            src="https://sprawdzam.afp.com/sites/default/files/styles/twitter_card/public/medias/factchecking/g2/2022-09/aa2dc4ac30e8a183448986495b465e19.jpeg?itok=GLOsh8mv"
-            variant="rounded"
-            sx={{ width: 80, height: 80 }}
-          />
-          <Stack
-            direction="column"
-            justifyContent="flex-start"
-            alignItems="flex-start"
-            spacing={2}
-          >
-            <Typography>{claim}</Typography>
-            <Typography> {title}</Typography>
-            <Typography>{rating}</Typography>
-            <Typography variant="subtitle2">{date}</Typography>
-          </Stack>
-          <Stack direction="column" spacing={2}>
-            <Link href="" variant="body1">
-              {website}
-            </Link>
-            <Chip label={language} />
-          </Stack>
-        </Stack>
+        ></Stack>
         <Divider orientation="horizontal" flexItem />
       </Box>
     );
@@ -74,6 +123,9 @@ const SemanticSearchResults = (searchResults) => {
               searchResults.date,
               searchResults.website,
               searchResults.language,
+              searchResults.similarityScore,
+              searchResults.articleUrl,
+              searchResults.domainUrl,
             )}
             {result(
               searchResults.claim,
@@ -82,6 +134,9 @@ const SemanticSearchResults = (searchResults) => {
               searchResults.date,
               searchResults.website,
               searchResults.language,
+              searchResults.similarityScore,
+              searchResults.articleUrl,
+              searchResults.domainUrl,
             )}
             <Box alignSelf="center" pt={4}>
               <Pagination count={10} color="primary" />
