bcc1d2cb7a1445f91a4e4b13a1733b8936c885e0
Valentin Porcellini <valentin.porcellini@afp.com>
2024-03-14 12:17:05 +0100

    Fixed lint errors

--- src/components/Shared/Utils/fileUtils.jsx
+++ src/components/Shared/Utils/fileUtils.jsx
@@ -11,47 +11,39 @@ export const FILE_TYPES = {
  * @param userRole The user role
  * @returns {boolean} True if the file is too large
  */
-export const isImageFileTooLarge = (imageFile, role) => {
+export const isImageFileTooLarge = (imageFile, userRole) => {
   if (!imageFile.type.includes("image")) {
     throw new Error("Invalid file type. This file is not an image.");
   }
 
-  if (
-    (!role || !role.includes("EXTRA_FEATURE")) &&
+  return (
+    (!userRole || !userRole.includes("EXTRA_FEATURE")) &&
     imageFile.size >= MAX_IMAGE_FILE_SIZE
-  ) {
-    return true;
-  }
-
-  return false;
+  );
 };
 
 /**
  * Helper function to check if the audio file is too large to be processed.
- * @param audioFile {file} The audio file to check
+ * @param audioFile {File} The audio file to check
  * @param userRole The user role
  * @returns {boolean} True if the file is too large
  */
-export const isAudioFileTooLarge = (audioFile, role) => {
+export const isAudioFileTooLarge = (audioFile, userRole) => {
   if (!audioFile.type.includes("audio")) {
     throw new Error("Invalid file type. This file is not an audio file.");
   }
 
-  if (
-    (!role || !role.includes("EXTRA_FEATURE")) &&
+  return (
+    (!userRole || !userRole.includes("EXTRA_FEATURE")) &&
     audioFile.size >= MAX_AUDIO_FILE_SIZE
-  ) {
-    return true;
-  }
-
-  return false;
+  );
 };
 
 /**
  * Preprocesses a file for file upload
  * @param file {File} The file to preprocess
  * @param role The user's role
- * @param preprocessingFn {File | undefined | Error } Optional additional preprocessing that can return a new preprocessed file or handle additionnal errors
+ * @param preprocessingFn {File | undefined | Error } Optional additional preprocessing that can return a new preprocessed file or handle additional errors
  * @param onSuccess {function} The function to run on preprocessing success
  * @param onError {function} The function to run in case of a preprocessing error
  * @returns {File | null | undefined} The processed file if the preprocessing succeeded
