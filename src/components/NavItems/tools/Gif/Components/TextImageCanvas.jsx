56780378a09caa745068b038e34859a4886f5128
robfrks <robin.frks@gmail.com>
2024-05-29 17:25:01 +0200

    can toggle fake image text and change its color

--- src/components/NavItems/tools/Gif/Components/TextImageCanvas.jsx
+++ src/components/NavItems/tools/Gif/Components/TextImageCanvas.jsx
@@ -1,10 +1,25 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, Fragment } from "react";
 import { Image, Layer, Rect, Stage, Text } from "react-konva";
 import { preloadImage } from "../../Forensic/utils";
+import {
+  Box,
+  Button,
+  FormControl,
+  FormControlLabel,
+  Grid,
+  InputLabel,
+  MenuItem,
+  Radio,
+  RadioGroup,
+  Select,
+  Typography,
+} from "@mui/material";
 
 const TextImageCanvas = ({ imgSrc, text, filterDataURL }) => {
   const stageRef = React.useRef(null);
   const [img, setImg] = useState(null);
+  const [textColor, setTextColor] = useState("red");
+  const [textActivated, setTextActivated] = useState(false);
 
   const handleExport = () => {
     if (!stageRef.current) return;
@@ -12,7 +27,13 @@ const TextImageCanvas = ({ imgSrc, text, filterDataURL }) => {
     filterDataURL(uri);
   };
 
-  const textBackground = () => {};
+  const changeColor = (event) => {
+    setTextColor(event.target.value);
+  };
+
+  const handleTextVisibility = () => {
+    setTextActivated(!textActivated);
+  };
 
   useEffect(() => {
     async function loadAndPreloadImage() {
@@ -30,18 +51,60 @@ const TextImageCanvas = ({ imgSrc, text, filterDataURL }) => {
     const height = 300 / imgRatio;
 
     return (
-      <Stage width={width} height={height}>
-        <Layer ref={stageRef}>
-          <Image image={img} width={width} height={height} />
-          <Text
-            text={text}
-            fontSize={40}
-            draggable
-            fill="red"
-            onDragEnd={handleExport}
-          />
-        </Layer>
-      </Stage>
+      <Grid
+        container
+        direction="row"
+        justifyContent="flex-start"
+        alignItems="center"
+      >
+        <Grid item>
+          <Stage width={width} height={height}>
+            <Layer ref={stageRef}>
+              <Image image={img} width={width} height={height} />
+              <Text
+                text={text}
+                fontSize={40}
+                draggable
+                fill={textColor}
+                onDragEnd={handleExport}
+                visible={textActivated}
+              />
+            </Layer>
+          </Stage>
+        </Grid>
+        <Grid item>
+          <Box m={2} />
+        </Grid>
+        <Grid item>
+          {text && (
+            <Fragment>
+              <Grid container direction="column">
+                <Grid item>
+                  <Button onClick={handleTextVisibility}>
+                    Add annotation to fake image
+                  </Button>
+                </Grid>
+                <Grid>
+                  <FormControl fullWidth>
+                    <InputLabel id="demo-simple-select-label">Color</InputLabel>
+                    <Select
+                      labelId="demo-simple-select-label"
+                      id="demo-simple-select"
+                      value={textColor}
+                      label="Age"
+                      onChange={changeColor}
+                    >
+                      <MenuItem value={"red"}>Red</MenuItem>
+                      <MenuItem value={"blue"}>Blue</MenuItem>
+                      <MenuItem value={"white"}>White</MenuItem>
+                    </Select>
+                  </FormControl>
+                </Grid>
+              </Grid>
+            </Fragment>
+          )}
+        </Grid>
+      </Grid>
     );
   } else {
     return;
