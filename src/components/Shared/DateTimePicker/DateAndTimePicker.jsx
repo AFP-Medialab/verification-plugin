c4b09c76b5df21124f501074df22c46fad4dc959
robfrks <robin.frks@gmail.com>
2024-09-16 17:16:10 +0200

    date picker disabled for past date

--- src/components/Shared/DateTimePicker/DateAndTimePicker.jsx
+++ src/components/Shared/DateTimePicker/DateAndTimePicker.jsx
@@ -81,6 +81,7 @@ const DateAndTimePicker = ({
               field: { clearable: true },
             }}
             disabled={disabled}
+            shouldDisableDate={pastDate}
           />
         </>
       )}
