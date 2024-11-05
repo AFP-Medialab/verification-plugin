c02fafd9f9c653379aff3afde423a42be2cdc7ff
Valentin Porcellini <valentin.porcellini@afp.com>
2024-11-05 13:57:58 +0100

    Added new transaction id, fixed permission issue and renamed it for clarity

--- src/constants/roles.jsx
+++ src/constants/roles.jsx
@@ -6,9 +6,9 @@
 export const ROLES = {
   ARCHIVE: "ARCHIVE",
   BETA_TESTER: "BETA_TESTER",
-  LOCK: "user",
+  REGISTERED_USER: "REGISTERED_USER", // Registered user base level
   EVALUATION: "EVALUATION",
   EXTRA_FEATURE: "EXTRA_FEATURE",
-  AFP_C2PA_GOLD: "AFP_C2PA_GOLD", //Highest level for AFP Reverse search
-  AFP_C2PA_2: "AFP_C2PA_2", //Lower level for AFP Reverse search
+  AFP_C2PA_GOLD: "AFP_C2PA_GOLD", // Highest level for AFP Reverse search
+  AFP_C2PA_2: "AFP_C2PA_2", // Lower level for AFP Reverse search
 };
