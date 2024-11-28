ca8cc3adba313d0a9a4e39d796d372520a784380
Michael Foster <m.foster@sheffield.ac.uk>
2024-11-28 11:14:49 +0000

    fixed slice 1

--- tests/e2e/assistant.spec.js
+++ tests/e2e/assistant.spec.js
@@ -1402,7 +1402,7 @@ const MediaServices = {
     mediaStatus: MediaVideoStatus.video,
     services: [MediaServices.videoDownload, MediaServices.metadata]
   },
-].slice(1).forEach(({
+].slice(0, 1).forEach(({
   url,
   videoGridIndex,
   imageGridIndex,
