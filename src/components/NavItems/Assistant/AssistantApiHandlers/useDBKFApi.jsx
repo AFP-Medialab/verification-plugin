bd5f21d7001ea0214b8a5e8ce032344905f15e74
Michael Foster <m.foster@sheffield.ac.uk>
2024-12-06 11:44:34 +0000

    Cleaning queries before submission to DBKF text api

--- src/components/NavItems/Assistant/AssistantApiHandlers/useDBKFApi.jsx
+++ src/components/NavItems/Assistant/AssistantApiHandlers/useDBKFApi.jsx
@@ -4,6 +4,14 @@ export default function DBKFApi() {
   const dbkfAPI = process.env.REACT_APP_DBKF_SEARCH_API;
   const similarityAPI = process.env.REACT_APP_DBKF_SIMILARITY_API;
 
+  function cleanQuery(query) {
+    // Remove the non alphanumeric suffixes, since the TextSimilarityEndpoint doesn't seem to like them
+    while (query.length > 0 && !/[a-zA-Z0-9]/.test(query[query.length - 1])) {
+      query = query.slice(0, -1);
+    }
+    return query;
+  }
+
   const callTextSimilarityEndpoint = async (query) => {
     /* if (... probably "when") this breaks: encodeURIComponent encodes line breaks, nbsp etc. according to utf-8
         https://www.w3schools.com/tags/ref_urlencode.ASP. but this call can't seem to handle any "space" except %20.
@@ -11,7 +19,7 @@ export default function DBKFApi() {
     let finalUri =
       dbkfAPI +
       "/documents?&limit=5&orderBy=score&q=" +
-      encodeURIComponent(query);
+      encodeURIComponent(cleanQuery(query));
     let searchResult = await axios.get(finalUri);
     let searchData = Object.values(searchResult.data);
     return searchData;
