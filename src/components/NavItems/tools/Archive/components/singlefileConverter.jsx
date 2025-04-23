68d1123307387eeb9c678d619c2544823b2c615b
Abbas Mahfouz <abbasmahfouzz@gmail.com>
2025-04-23 10:24:11 +0200

    delete space

--- src/components/NavItems/tools/Archive/components/singlefileConverter.jsx
+++ src/components/NavItems/tools/Archive/components/singlefileConverter.jsx
@@ -408,7 +408,7 @@ const SinglefileConverter = (telegramURL) => {
           "WARC-Date": date,
           "WARC-Type": type,
           "WARC-Record-ID": `<urn:uuid:${uuid()}>`,
-          "Content-Type": "text/html; charset=UTF-8",
+          "Content-Type": "text/html;charset=UTF-8",
         },
       },
       content(),
