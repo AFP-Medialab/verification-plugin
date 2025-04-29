2a880ae5f7f3da25ee5355f8ee1a22ed287b768d
Michael Foster <m.foster@sheffield.ac.uk>
2025-04-29 09:40:41 +0100

    Changed max image size to 4MB

--- src/config/index.jsx
+++ src/config/index.jsx
@@ -1,5 +1,5 @@
 //Max image file size in bytes for the tools
-export const MAX_IMAGE_FILE_SIZE = 15000000;
+export const MAX_IMAGE_FILE_SIZE = 4000000;
 
 //Max audio file size in bytes for the tools
 export const MAX_AUDIO_FILE_SIZE = 15000000;
