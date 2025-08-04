640e85596bbd08266fdd75cd75ce25005c251db9
Rosanna Milner <44234896+rosannamilner@users.noreply.github.com>
2025-08-04 15:30:34 +0100

    removed more unused code

--- src/components/NavItems/Assistant/AssistantScrapeResults/AssistantWarnings.jsx
+++ src/components/NavItems/Assistant/AssistantScrapeResults/AssistantWarnings.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 
 import Alert from "@mui/material/Alert";
 import Card from "@mui/material/Card";
@@ -17,15 +17,11 @@ import { ROLES } from "@/constants/roles";
 const AssistantWarnings = () => {
   const keyword = i18nLoadNamespace("components/NavItems/tools/Assistant");
   const classes = useMyStyles();
-  const dispatch = useDispatch();
 
   // checking if user logged in
   const role = useSelector((state) => state.userSession.user.roles);
 
   // state
-  const warningExpanded = useSelector(
-    (state) => state.assistant.warningExpanded,
-  );
   const dbkfTextMatch = useSelector((state) => state.assistant.dbkfTextMatch);
   const dbkfImageMatch = useSelector((state) => state.assistant.dbkfImageMatch);
   const dbkfVideoMatch = useSelector((state) => state.assistant.dbkfVideoMatch);
