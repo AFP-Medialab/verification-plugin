8987311d5b549ba404c37894958c598e3e6d9e5d
Rosanna Milner <44234896+rosannamilner@users.noreply.github.com>
2025-08-04 15:28:19 +0100

    fix title for DBKF and remove unused imports

--- src/components/NavItems/Assistant/AssistantScrapeResults/AssistantWarnings.jsx
+++ src/components/NavItems/Assistant/AssistantScrapeResults/AssistantWarnings.jsx
@@ -2,18 +2,10 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import Alert from "@mui/material/Alert";
-import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardHeader from "@mui/material/CardHeader";
-import CardMedia from "@mui/material/CardMedia";
-import Collapse from "@mui/material/Collapse";
-import Grid from "@mui/material/Grid";
-import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
-
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import WarningAmber from "@mui/icons-material/WarningAmber";
 
 import DbkfMediaResults from "@/components/NavItems/Assistant/AssistantCheckResults/DbkfMediaResults";
 import DbkfTextResults from "@/components/NavItems/Assistant/AssistantCheckResults/DbkfTextResults";
@@ -21,7 +13,6 @@ import PreviousFactCheckResults from "@/components/NavItems/Assistant/AssistantC
 import { i18nLoadNamespace } from "@/components/Shared/Languages/i18nLoadNamespace";
 import useMyStyles from "@/components/Shared/MaterialUiStyles/useMyStyles";
 import { ROLES } from "@/constants/roles";
-import { setWarningExpanded } from "@/redux/actions/tools/assistantActions";
 
 const AssistantWarnings = () => {
   const keyword = i18nLoadNamespace("components/NavItems/tools/Assistant");
@@ -48,7 +39,7 @@ const AssistantWarnings = () => {
         className={classes.assistantCardHeader}
         title={
           <Alert severity="warning" sx={{ bgcolor: "background.paper" }}>
-            <Typography>Database of known fakes matches</Typography>
+            <Typography>{keyword("dbkf_title")}</Typography>
           </Alert>
         }
       />
