6f97646bde9df7ee296f5cae8fd8b99a506098b3
Rosanna Milner <rosanna.milner@sheffield.ac.uk>
2025-09-24 08:44:38 +0100

    remove index from GaugeChartModalExplanation

--- src/components/NavItems/Assistant/AssistantScrapeResults/assistantUtils.jsx
+++ src/components/NavItems/Assistant/AssistantScrapeResults/assistantUtils.jsx
@@ -7,7 +7,7 @@ import ListItem from "@mui/material/ListItem";
 import Slider from "@mui/material/Slider";
 import Typography from "@mui/material/Typography";
 
-import GaugeChartModalExplanation from "@Shared/GaugeChartResults/GaugeChartModalExplanation/index";
+import GaugeChartModalExplanation from "@Shared/GaugeChartResults/GaugeChartModalExplanation";
 import _ from "lodash";
 import { v4 as uuidv4 } from "uuid";
 
