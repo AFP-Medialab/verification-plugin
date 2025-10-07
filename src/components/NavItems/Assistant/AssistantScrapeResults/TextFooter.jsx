964655e6178c6eeaade2fafc594439a5317b7103
Rosanna Milner <44234896+rosannamilner@users.noreply.github.com>
2025-10-07 09:27:26 +0100

    remove expand button from extracted text footer (#948)

--- src/components/NavItems/Assistant/AssistantScrapeResults/TextFooter.jsx
+++ src/components/NavItems/Assistant/AssistantScrapeResults/TextFooter.jsx
@@ -2,32 +2,22 @@ import React from "react";
 import { useSelector } from "react-redux";
 
 import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
 import Divider from "@mui/material/Divider";
 import Grid from "@mui/material/Grid";
-import IconButton from "@mui/material/IconButton";
-import Link from "@mui/material/Link";
 import Tooltip from "@mui/material/Tooltip";
 import Typography from "@mui/material/Typography";
-
-import { ExpandLessOutlined, ExpandMoreOutlined } from "@mui/icons-material";
 
 import { i18nLoadNamespace } from "@Shared/Languages/i18nLoadNamespace";
 import { TextCopy } from "@Shared/Utils/TextCopy";
 import { Translate } from "@Shared/Utils/Translate";
 import { getLanguageName } from "@Shared/Utils/languageUtils";
 
-import { scrollToElement } from "./assistantUtils";
-
 export default function TextFooter({
   classes,
   setDisplayOrigLang,
   displayOrigLang,
   textLang,
-  expandMinimiseText,
   text,
-  setExpanded,
-  expanded,
 }) {
   const keyword = i18nLoadNamespace("components/Shared/utils");
   const locale = useSelector((state) => state.language);
@@ -37,7 +27,7 @@ export default function TextFooter({
       <Grid container spacing={2}>
         {/* language detected */}
         <Grid
-          size={9}
+          size={10}
           align={"left"}
           sx={{
             pt: 1,
@@ -85,86 +75,7 @@ export default function TextFooter({
             </div>
           </Tooltip>
         </Grid>
-
-        {/* expand/minimise text */}
-        <Grid
-          size={{ xs: 1 }}
-          align={"right"}
-          sx={{
-            display: "flex",
-            pt: 1,
-          }}
-        >
-          <ExpandMinimise
-            classes={classes}
-            expandMinimiseText={expandMinimiseText}
-            setExpanded={setExpanded}
-            expanded={expanded}
-          />
-        </Grid>
       </Grid>
     </Box>
-  );
-}
-
-export function ExpandMinimise({
-  classes,
-  expandMinimiseText,
-  setExpanded,
-  expanded,
-  type,
-}) {
-  return type === "BUTTON" ? (
-    <Button
-      variant="outlined"
-      color="primary"
-      size={"large"}
-      fullWidth
-      onClick={() => {
-        setExpanded(!expanded);
-        expanded ? scrollToElement("credibility-signals", 100) : null;
-      }}
-    >
-      {expanded ? (
-        <ExpandLessOutlined
-          className={classes.toolTipIcon}
-          color="primary"
-          style={{ display: "inline-flex" }}
-        />
-      ) : (
-        <ExpandMoreOutlined
-          className={classes.toolTipIcon}
-          color="primary"
-          style={{ display: "inline-flex" }}
-        />
-      )}
-      {expandMinimiseText}
-    </Button>
-  ) : (
-    <IconButton
-      onClick={() => {
-        setExpanded(!expanded);
-        expanded ? scrollToElement("credibility-signals", 100) : null;
-      }}
-      sx={{
-        "&:hover": {
-          backgroundColor: "inherit",
-        },
-      }}
-    >
-      {expanded ? (
-        <ExpandLessOutlined
-          className={classes.toolTipIcon}
-          color="primary"
-          style={{ display: "inline-flex" }}
-        />
-      ) : (
-        <ExpandMoreOutlined
-          className={classes.toolTipIcon}
-          color="primary"
-          style={{ display: "inline-flex" }}
-        />
-      )}
-    </IconButton>
   );
 }
