c122bc31bae10e452857b2d68e13132d094c0415
Rosanna Milner <rosanna.milner@sheffield.ac.uk>
2025-11-12 13:55:35 +0000

    uncomment dragging files to upload

--- src/components/NavItems/Assistant/FileUploadField.jsx
+++ src/components/NavItems/Assistant/FileUploadField.jsx
@@ -1,26 +1,21 @@
-import React, {
-  useRef,
-  // useState,
-} from "react";
+import React, { useRef, useState } from "react";
 
-// import Box from "@mui/material/Box";
+import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import ButtonGroup from "@mui/material/ButtonGroup";
 import Grid from "@mui/material/Grid";
-
-// import Stack from "@mui/material/Stack";
-// import Typography from "@mui/material/Typography";
-// import { green } from "@mui/material/colors";
-// import { useColorScheme } from "@mui/material/styles";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+import { green } from "@mui/material/colors";
+import { useColorScheme } from "@mui/material/styles";
 
 import CloseIcon from "@mui/icons-material/Close";
 import FolderOpenIcon from "@mui/icons-material/FolderOpen";
 
-// import accept from "attr-accept";
+import { i18nLoadNamespace } from "@Shared/Languages/i18nLoadNamespace";
+import accept from "attr-accept";
 
 import { prettifyLargeString } from "../../NavItems/tools/Archive/utils";
-
-// import { i18nLoadNamespace } from "@Shared/Languages/i18nLoadNamespace";
 
 /**
  * A reusable form component with a textfield and a local file with optional processing
@@ -54,36 +49,36 @@ const FileUploadField = ({
 }) => {
   const fileRef = useRef(null);
 
-  //   const [isDragging, setIsDragging] = useState(false);
-  //   const [validDrop, setValidDrop] = useState(false);
+  const [isDragging, setIsDragging] = useState(false);
+  const [validDrop, setValidDrop] = useState(false);
 
-  //   const { mode, systemMode } = useColorScheme();
-  //   const resolvedMode = systemMode || mode;
+  const { mode, systemMode } = useColorScheme();
+  const resolvedMode = systemMode || mode;
 
-  //   const dropColor = resolvedMode === "light" ? green[50] : green[900];
+  const dropColor = resolvedMode === "light" ? green[50] : green[900];
 
-  //   const keyword = i18nLoadNamespace("components/Shared/StringFileUploadField");
+  const keyword = i18nLoadNamespace("components/Shared/StringFileUploadField");
 
-  //   /**
-  //    *
-  //    * @param e {DragEvent}
-  //    */
-  //   const onDragOver = (e) => {
-  //     e.preventDefault();
-  //     const file = e.dataTransfer?.files?.[0];
-  //     setIsDragging(true);
+  /**
+   *
+   * @param e {DragEvent}
+   */
+  const onDragOver = (e) => {
+    e.preventDefault();
+    const file = e.dataTransfer?.files?.[0];
+    setIsDragging(true);
 
-  //     if (file && accept(file, fileInputTypesAccepted)) {
-  //       setValidDrop(true);
-  //     } else {
-  //       setValidDrop(false);
-  //     }
-  //   };
+    if (file && accept(file, fileInputTypesAccepted)) {
+      setValidDrop(true);
+    } else {
+      setValidDrop(false);
+    }
+  };
 
-  //   const onDragLeave = (e) => {
-  //     e.preventDefault();
-  //     setIsDragging(false);
-  //   };
+  const onDragLeave = (e) => {
+    e.preventDefault();
+    setIsDragging(false);
+  };
 
   const handleFile = async (file) => {
     if (preprocessLocalFile) {
@@ -92,125 +87,125 @@ const FileUploadField = ({
     setFileInput(file);
   };
 
-  //   const onDrop = (e) => {
-  //     e.preventDefault();
-  //     setIsDragging(false);
-  //     setValidDrop(false);
-  //     const file = e.dataTransfer?.files?.[0];
-  //     if (file && accept(file, fileInputTypesAccepted)) {
-  //       handleFile(file);
-  //     }
-  //   };
+  const onDrop = (e) => {
+    e.preventDefault();
+    setIsDragging(false);
+    setValidDrop(false);
+    const file = e.dataTransfer?.files?.[0];
+    if (file && accept(file, fileInputTypesAccepted)) {
+      handleFile(file);
+    }
+  };
 
   return (
-    // <Box
-    //   onDragOver={onDragOver}
-    //   onDragLeave={onDragLeave}
-    //   onDrop={onDrop}
-    //   sx={{
-    //     position: "relative",
-    //   }}
-    // >
-    //   {isDragging && (
-    //     <Stack
-    //       sx={{
-    //         height: "100%",
-    //         border: "4px dashed var(--mui-palette-primary-main)",
-    //         backgroundColor: dropColor,
-    //         justifyContent: "center",
-    //         alignItems: "center",
-    //         position: "absolute",
-    //         top: 0,
-    //         left: 0,
-    //         bottom: 0,
-    //         right: 0,
-    //       }}
-    //     >
-    //       <Typography>{keyword("droppable_zone")}</Typography>
-    //     </Stack>
-    //   )}
-    //   <Box
-    //     sx={{
-    //       visibility: isDragging ? "hidden" : "visible",
-    //     }}
-    //   >
-    //   </Box>
-    // </Box>
-    <Grid
-      container
-      direction="row"
-      spacing={3}
+    <Box
+      onDragOver={onDragOver}
+      onDragLeave={onDragLeave}
+      onDrop={onDrop}
       sx={{
-        alignItems: "center",
+        position: "relative",
       }}
     >
-      <Grid>
-        <ButtonGroup
-          variant="outlined"
-          // disabled={isParentLoading || urlInput !== ""}
-        >
-          <Button
-            startIcon={<FolderOpenIcon />}
-            // style={
-            //     isDragging
-            //     ? { cursor: validDrop ? "copy" : "no-drop" }
-            //     : undefined
-            // }
-            // onDragOver={onDragOver}
-            // onDragLeave={onDragLeave}
-            // onDrop={onDrop}
-          >
-            <label htmlFor="file">
-              {fileInput
-                ? prettifyLargeString(fileInput.name, 24)
-                : localFileKeyword}
-            </label>
-            <input
-              id="file"
-              name="file"
-              type="file"
-              accept={fileInputTypesAccepted}
-              hidden={true}
-              ref={fileRef}
-              onChange={(e) => {
-                e.preventDefault();
-                handleFile(e.target.files[0]);
-                e.target.value = null;
-              }}
-            />
-          </Button>
-          {fileInput instanceof Blob && (
-            <Button
-              size="small"
-              aria-label="remove selected file"
-              onClick={(e) => {
-                e.preventDefault();
-                handleCloseSelectedFile();
-                fileRef.current.value = null;
-                setFileInput(null);
-              }}
-            >
-              <CloseIcon fontSize="small" />
-            </Button>
-          )}
-        </ButtonGroup>
-      </Grid>
-      <Grid>
-        <Button
-          type="submit"
-          variant="contained"
-          color="primary"
-          onClick={async (e) => {
-            e.preventDefault();
-            await handleSubmit(e);
+      {isDragging && (
+        <Stack
+          sx={{
+            height: "100%",
+            border: "4px dashed var(--mui-palette-primary-main)",
+            backgroundColor: dropColor,
+            justifyContent: "center",
+            alignItems: "center",
+            position: "absolute",
+            top: 0,
+            left: 0,
+            bottom: 0,
+            right: 0,
           }}
-          loading={isParentLoading}
-          //   disabled={(urlInput === "" && !fileInput) || isParentLoading}
         >
-          {submitButtonKeyword}
-        </Button>
-      </Grid>
-    </Grid>
+          <Typography>{keyword("droppable_zone")}</Typography>
+        </Stack>
+      )}
+      <Box
+        sx={{
+          visibility: isDragging ? "hidden" : "visible",
+        }}
+      >
+        <Grid
+          container
+          direction="row"
+          spacing={3}
+          sx={{
+            alignItems: "center",
+          }}
+        >
+          <Grid>
+            <ButtonGroup
+              variant="outlined"
+              // disabled={isParentLoading || urlInput !== ""}
+            >
+              <Button
+                startIcon={<FolderOpenIcon />}
+                style={
+                  isDragging
+                    ? { cursor: validDrop ? "copy" : "no-drop" }
+                    : undefined
+                }
+                onDragOver={onDragOver}
+                onDragLeave={onDragLeave}
+                onDrop={onDrop}
+              >
+                <label htmlFor="file">
+                  {fileInput
+                    ? prettifyLargeString(fileInput.name, 24)
+                    : localFileKeyword}
+                </label>
+                <input
+                  id="file"
+                  name="file"
+                  type="file"
+                  accept={fileInputTypesAccepted}
+                  hidden={true}
+                  ref={fileRef}
+                  onChange={(e) => {
+                    e.preventDefault();
+                    handleFile(e.target.files[0]);
+                    e.target.value = null;
+                  }}
+                />
+              </Button>
+              {fileInput instanceof Blob && (
+                <Button
+                  size="small"
+                  aria-label="remove selected file"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    handleCloseSelectedFile();
+                    fileRef.current.value = null;
+                    setFileInput(null);
+                  }}
+                >
+                  <CloseIcon fontSize="small" />
+                </Button>
+              )}
+            </ButtonGroup>
+          </Grid>
+          <Grid>
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              onClick={async (e) => {
+                e.preventDefault();
+                await handleSubmit(e);
+              }}
+              loading={isParentLoading}
+              //   disabled={(urlInput === "" && !fileInput) || isParentLoading}
+            >
+              {submitButtonKeyword}
+            </Button>
+          </Grid>
+        </Grid>
+      </Box>
+    </Box>
   );
 };
 
