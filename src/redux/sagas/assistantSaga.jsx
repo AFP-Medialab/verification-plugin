343ea64ac5510e493d3037843bc21c2a49c36255
Rosanna Milner <rosanna.milner@sheffield.ac.uk>
2026-02-04 15:13:45 +0000

    add missing yield to userAuthenticated

--- src/redux/sagas/assistantSaga.jsx
+++ src/redux/sagas/assistantSaga.jsx
@@ -163,7 +163,7 @@ function* handleMediaActionList() {
   const processUrl = yield select((state) => state.assistant.processUrl);
   const contentType = yield select((state) => state.assistant.processUrlType);
   const role = yield select((state) => state.userSession.user.roles);
-  const userAuthenticated = select(
+  const userAuthenticated = yield select(
     (state) => state.userSession.userAuthenticated,
   );
   if (processUrl !== null) {
