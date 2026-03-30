1d5a22525633509761ae30e8843b37555454f641
Rosanna Milner <rosanna.milner@sheffield.ac.uk>
2026-03-30 15:50:00 +0100

    remove mgt sentences from assistant, dispaly chunk results only

--- src/redux/reducers/assistantReducer.jsx
+++ src/redux/reducers/assistantReducer.jsx
@@ -69,11 +69,6 @@ const defaultState = {
   machineGeneratedTextChunksDone: false,
   machineGeneratedTextChunksFail: false,
 
-  machineGeneratedTextSentencesResult: null,
-  machineGeneratedTextSentencesLoading: false,
-  machineGeneratedTextSentencesDone: false,
-  machineGeneratedTextSentencesFail: false,
-
   multilingualStanceResult: null,
   multilingualStanceLoading: false,
   multilingualStanceDone: false,
@@ -108,7 +103,6 @@ const assistantReducer = (state = defaultState, action) => {
     case "SET_SUBJECTIVITY_DETAILS":
     case "SET_PREV_FACT_CHECKS_DETAILS":
     case "SET_MACHINE_GENERATED_TEXT_CHUNKS_DETAILS":
-    case "SET_MACHINE_GENERATED_TEXT_SENTENCES_DETAILS":
     case "SET_MULTILINGUAL_STANCE_DETAILS":
     case "SET_LOADING":
     case "SET_MISSING_MEDIA":
@@ -202,11 +196,6 @@ const assistantReducer = (state = defaultState, action) => {
         machineGeneratedTextChunksLoading: false,
         machineGeneratedTextChunksDone: false,
         machineGeneratedTextChunksFail: false,
-
-        machineGeneratedTextSentencesResult: null,
-        machineGeneratedTextSentencesLoading: false,
-        machineGeneratedTextSentencesDone: false,
-        machineGeneratedTextSentencesFail: false,
 
         multilingualStanceResult: null,
         multilingualStanceLoading: false,
