9b2343a79e8ac9572ab404e09f44947da509941d
Thomas Guillet <guilletthomas59@gmail.com>
2026-07-30 10:18:20 +0200

    replace osm tile by carto db #1241

--- src/components/NavItems/tools/Geolocation/Results/GeolocationResults.jsx
+++ src/components/NavItems/tools/Geolocation/Results/GeolocationResults.jsx
@@ -140,8 +140,8 @@ const GeolocationResults = ({ result, urlImage }) => {
                             }}
                           >
                             <TileLayer
-                              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+                              url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
                             />
                             <Marker
                               position={[res.latitude, res.longitude]}
